9949f3f807188d93acbd04f402aef29c988a62cc
Erik Serating <serating@gmail.com>
2013-04-10 16:49:01 -0400

    More updates to converter part2

---
+++ src/test/java/com/google/code/siren4j/component/testpojos/BasePojo.java
@@ -0,0 +1,20 @@
+package com.google.code.siren4j.component.testpojos;
+
+import com.google.code.siren4j.annotations.SirenPropertyIgnore;
+
+public class BasePojo {
+	
+	
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+
+}
